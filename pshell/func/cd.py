842b314bc49f20b98fcda5fbed0b0156dabdcb58
huanzhewu <546108715@qq.com>
2017-04-29 01:40:32 -0700

    注释

--- pshell/func/cd.py
+++ pshell/func/cd.py
@@ -1,9 +1,11 @@
 from .constants import *
+# os.chdir(args) will change the work directory into args
 
 def cd(args):
     if len(args) >0:
         os.chdir(args[0])
     else:
+        # if args is null , will change directory into user's home 
         os.chdir(os.getenv('HOME'))
     return SHELL_STATUS_RUN
 
--- pshell/func/constants.py
+++ pshell/func/constants.py
@@ -2,5 +2,10 @@ import os
 SHELL_STATUS_RUN = 1
 SHELL_STATUS_STOP = 0
 
-HISTORY_PATH = os.path.expanduser('~')+os.sep + '.shiyanlou_shell_history'
+# os.path.expanduser('~') will replace ~ to user's home directory,like /home/ortonwu
 
+# os.sep is equal to '/'
+
+# for my mechine ,HISTORY_PATH is /home/ortonwu/.ortonwu_shell_history
+
+HISTORY_PATH = os.path.expanduser('~')+os.sep + '.ortonwu_shell_history'
