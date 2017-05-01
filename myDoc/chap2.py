8b2cc73abd27928dbf579858e81b3fb9b27ea406
huanzhewu <546108715@qq.com>
2017-05-01 02:22:39 -0700

    默认参数

--- myDoc/chap2.py
+++ myDoc/chap2.py
@@ -23,14 +23,37 @@ def returnMore():
     return a,b
 
 
+def addressBook(name , phone ,Class="计算机科学与技术3班 "):
+    print('name:',name)
+    print('phone:',phone)
+    print('Class:',Class)
+
+def func(a,L=[]):
+    L.append(a)
+    return L
+
+def func1(a=0):
+    a += 1
+    return a
+
 if  __name__ == '__main__':
     # error call
-    #fib(1,2) 
+    #fib(1,2)
+
     #print(fib(10))
+    
     #arr = getfib(10)
     
-   
-    a =  returnMore()
-    print(a)
+    #a =  returnMore()
+    #print(a)
     
    # print(arr)
+
+  #  addressBook('小明',785496)
+  #  addressBook('小红',121545)
+  #  addressBook('小黑',548796,"网络工程1班")
+
+    for i in range(10):
+       print( func1(i) )
+
+
