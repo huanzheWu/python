c56800985099f4a2b6b6ef01447da41876bd4a58
huanzhewu <546108715@qq.com>
2017-05-01 00:07:00 -0700

    定义函数

---
+++ myDoc/chap2.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python
+def fib(n):
+    """ 打印斐波那契数列的前N项"""
+    a,b=0,1
+    for i in range(n):
+        print(a,end=' ')
+        a ,b = b,a+b
+
+    print()
+
+if  __name__ == '__main__':
+    
+    fib(10)
